7d1ec0b65a9fdc7cdbc652faf882aa40f1233a21
MAITUMELO RADIPODI <mradipodi@gmail.com>
2024-03-13 12:04:16 +0200

    get student by id functional

--- backend/Server.js
+++ backend/Server.js
@@ -6,9 +6,9 @@ const PORT = 8000;
 //dummy data
 let students = [
   { id: 1, name: 'John', lastname: 'Jonnie', age: 20, class: '1A' },
-  { id: 1, name: 'May', lastname: 'Jon', age: 20, class: '1A' },
-  { id: 1, name: 'June', lastname: 'May', age: 20, class: '1A' },
-  { id: 1, name: 'Rati', lastname: 'Doe', age: 20, class: '1A' }
+  { id: 2, name: 'May', lastname: 'Jon', age: 20, class: '1A' },
+  { id: 3, name: 'June', lastname: 'May', age: 20, class: '1A' },
+  { id: 4, name: 'Rati', lastname: 'Doe', age: 20, class: '1A' }
 ];
 
 app.use(bodyParser.json());
@@ -18,6 +18,17 @@ app.use(bodyParser.json());
 //Get all students
 app.get('/student', (req, res) => {
   res.json(students);
+});
+
+// get a student using a specific id
+app.get('/student/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const student = students.find((student) => student.id === id);
+  if (student) {
+    res.json(student);
+  } else {
+    res.status(404).json({ error: 'Student not found' });
+  }
 });
 
 app.listen(PORT, () => {
